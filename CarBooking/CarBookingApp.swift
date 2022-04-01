2672355374e98ba5a28c8f97d40d140cda427dce
You <dzalolx@gmail.com>
2022-04-01 21:05:55 +0200

    Initial Commit

---
+++ CarBooking/CarBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarBookingApp.swift
+//  CarBooking
+//
+//  Created by Robert Adamczyk on 01.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CarBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
