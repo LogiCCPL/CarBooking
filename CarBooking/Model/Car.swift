7fb8838db5ba1ae1c1f9b7126f268dddca784ca5
You <dzalolx@gmail.com>
2022-04-01 21:50:29 +0200

    Created: - model - developer preview

---
+++ CarBooking/Model/Car.swift
@@ -0,0 +1,21 @@
+//
+//  Car.swift
+//  CarBooking
+//
+//  Created by Robert Adamczyk on 01.04.22.
+//
+
+import Foundation
+struct Car: Codable {
+    var id: Int
+    var name: String
+    var shortDescription: String?
+}
+
+struct CarDetail: Codable{
+    var id: Int
+    var name: String
+    var shortDescription: String?
+    var description: String
+    var image: String
+}
---
+++ CarBooking/Model/DeveloperPreview.swift
@@ -0,0 +1,38 @@
+//
+//  DeveloperPreview.swift
+//  CarBooking
+//
+//  Created by Robert Adamczyk on 01.04.22.
+//
+
+import Foundation
+
+class DeveloperPreview {
+    static let instance = DeveloperPreview()
+    
+    let car = Car(id: 1, name: "intive_Kupferwerk 1", shortDescription: "Limousine with 5 Seats")
+    
+    let carList: [Car] = [
+        Car(id: 1, name: "intive_Kupferwerk 1", shortDescription: "Limousine with 5 Seats"),
+        Car(id: 1, name: "intive_Kupferwerk 1", shortDescription: "Limousine with 5 Seats"),
+        Car(id: 1, name: "intive_Kupferwerk 1", shortDescription: "Limousine with 5 Seats"),
+        Car(id: 1, name: "intive_Kupferwerk 1", shortDescription: "Limousine with 5 Seats")
+    ]
+    
+    let carDetail = CarDetail(
+        id: 1,
+        name: "intive_Kupferwerk",
+        shortDescription: "Limousine with 5 Seats",
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        image: "images/automotive.jpg")
+}
+
+/*
+ 
+ "id" : 1,
+   "name" : "intive_Kupferwerk 1",
+   "shortDescription" : "Limousine with 5 Seats",
+   "description" : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+   "image" : "images/automotive.jpg"
+ 
+ */
